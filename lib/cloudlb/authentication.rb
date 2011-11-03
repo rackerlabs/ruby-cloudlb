6448657908694ccab83c5a4d37af1d298b289caa
Thomas Haggett <thomas-github@haggett.org>
2011-11-03 11:58:35 +0000

    Don't disable SSL peer verification!

--- lib/cloudlb/authentication.rb
+++ lib/cloudlb/authentication.rb
@@ -12,7 +12,6 @@ module CloudLB
         :method                        => :get,
         :headers                       => { "X-Auth-User" => connection.authuser, "X-Auth-Key" => connection.authkey },
         :user_agent                    => "Cloud Load Balancers Ruby API #{VERSION}",
-        :disable_ssl_peer_verification => true,
         :verbose                       => ENV['LOADBALANCERS_VERBOSE'] ? true : false)
       CloudLB.hydra.queue(request)
       CloudLB.hydra.run
